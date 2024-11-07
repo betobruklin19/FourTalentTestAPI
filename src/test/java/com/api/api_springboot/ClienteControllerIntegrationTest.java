90dc11ea5dc2821bbae34312313a75e443c4d59b
betobruklin19 <robertosilva1991@outlook.com>
2024-11-07 10:39:43 -0300

    Correção dos testes de integração após autenticação do security

--- src/test/java/com/api/api_springboot/ClienteControllerIntegrationTest.java
+++ src/test/java/com/api/api_springboot/ClienteControllerIntegrationTest.java
@@ -5,13 +5,20 @@ import com.api.api_springboot.entities.Endereco;
 import com.api.api_springboot.enums.Estado;
 import com.api.api_springboot.repositories.ClienteRepository;
 import com.api.api_springboot.services.ClienteService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -29,12 +36,29 @@ public class ClienteControllerIntegrationTest {
     @Autowired
     private ClienteService clienteService;
 
+    private String jwtToken;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        String loginJson = "{\"login\":\"betobruklin\",\"password\":\"12345678\"}";
+
+        jwtToken = mockMvc.perform(MockMvcRequestBuilders.post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginJson))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+    }
+
+
     @Test
     void cadastrarCliente_retornaClienteCriado() throws Exception {
         String clienteJson = "{\"nome\":\"João\",\"email\":\"joao@example.com\",\"endereco\":{\"rua\":\"Rua A\",\"cidade\":\"Cidade X\",\"estado\":\"SP\"}}";
 
         mockMvc.perform(MockMvcRequestBuilders.post("/clientes")
                         .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
                         .content(clienteJson))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.nome", is("João")))
@@ -43,18 +67,18 @@ public class ClienteControllerIntegrationTest {
 
     @Test
     void buscarClientePorId_clienteExistente_retornaCliente() throws Exception {
-        Endereco endereco = new Endereco();
-        endereco.setRua("Rua B");
-        endereco.setCidade("Cidade Y");
-        endereco.setEstado(Estado.SP);
+        String clienteJson = "{\"nome\":\"Maria\",\"email\":\"maria@example.com\",\"endereco\":{\"rua\":\"Rua A\",\"cidade\":\"Cidade X\",\"estado\":\"SP\"}}";
 
-        Cliente cliente = new Cliente();
-        cliente.setNome("Maria");
-        cliente.setEmail("maria@example.com");
-        cliente.setEndereco(endereco);
-        clienteService.cadastrarCliente(cliente);
+        mockMvc.perform(MockMvcRequestBuilders.post("/clientes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                        .content(clienteJson));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/clientes/" + cliente.getId()))
+        Optional<Cliente> cliente = clienteRepository.buscarClientePorEmail("maria@example.com");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/clientes/" + cliente.get().getId())
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.nome", is("Maria")))
                 .andExpect(jsonPath("$.email", is("maria@example.com")));
@@ -62,61 +86,58 @@ public class ClienteControllerIntegrationTest {
 
     @Test
     void listarTodosClientes_retornaListaDeClientes() throws Exception {
-        Endereco endereco = new Endereco();
-        endereco.setRua("Rua C");
-        endereco.setCidade("Cidade Z");
-        endereco.setEstado(Estado.SP);
+        String clienteJson = "{\"nome\":\"Carlos\",\"email\":\"carlos@example.com\",\"endereco\":{\"rua\":\"Rua A\",\"cidade\":\"Cidade X\",\"estado\":\"SP\"}}";
 
-        Cliente cliente = new Cliente();
-        cliente.setNome("Cliente Teste");
-        cliente.setEmail("cliente@example.com");
-        cliente.setEndereco(endereco);
-        clienteRepository.salvarCliente(cliente);
+        mockMvc.perform(MockMvcRequestBuilders.post("/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                .content(clienteJson));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/clientes"))
+        mockMvc.perform(MockMvcRequestBuilders.get("/clientes")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()", greaterThanOrEqualTo(1)));
     }
 
     @Test
     void atualizarCliente_clienteExistente_atualizaCliente() throws Exception {
-        Endereco endereco = new Endereco();
-        endereco.setRua("Rua D");
-        endereco.setCidade("Cidade W");
-        endereco.setEstado(Estado.SP);
+        String clienteJson = "{\"nome\":\"Josafa\",\"email\":\"josafa@example.com\",\"endereco\":{\"rua\":\"Rua A\",\"cidade\":\"Cidade X\",\"estado\":\"SP\"}}";
 
-        Cliente cliente = new Cliente();
-        cliente.setNome("Carlos");
-        cliente.setEmail("carlos@example.com");
-        cliente.setEndereco(endereco);
-        clienteRepository.salvarCliente(cliente);
+        mockMvc.perform(MockMvcRequestBuilders.post("/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                .content(clienteJson));
 
-        String clienteAtualizadoJson = "{\"nome\":\"Carlos Atualizado\",\"email\":\"carlosatualizado@example.com\"," +
+        Optional<Cliente> cliente = clienteRepository.buscarClientePorEmail("josafa@example.com");
+
+
+        String clienteAtualizadoJson = "{\"nome\":\"Josafa Atualizado\",\"email\":\"josafaatualizado@example.com\"," +
                 "\"endereco\":{\"rua\":\"Rua D\",\"cidade\":\"Cidade W\",\"estado\":\"SP\"}}";
 
-
-        mockMvc.perform(MockMvcRequestBuilders.put("/clientes/" + cliente.getId())
+        mockMvc.perform(MockMvcRequestBuilders.put("/clientes/" + cliente.get().getId())
                         .contentType(MediaType.APPLICATION_JSON)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
                         .content(clienteAtualizadoJson))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.nome", is("Carlos Atualizado")))
-                .andExpect(jsonPath("$.email", is("carlosatualizado@example.com")));
+                .andExpect(jsonPath("$.nome", is("Josafa Atualizado")))
+                .andExpect(jsonPath("$.email", is("josafaatualizado@example.com")));
     }
 
     @Test
     void deletarCliente_clienteExistente_retornaNoContent() throws Exception {
-        Endereco endereco = new Endereco();
-        endereco.setRua("Rua D");
-        endereco.setCidade("Cidade W");
-        endereco.setEstado(Estado.SP);
+        String clienteJson = "{\"nome\":\"Cornelio\",\"email\":\"cornelio@example.com\",\"endereco\":{\"rua\":\"Rua A\",\"cidade\":\"Cidade X\",\"estado\":\"SP\"}}";
 
-        Cliente cliente = new Cliente();
-        cliente.setNome("Laura");
-        cliente.setEmail("laura@example.com");
-        cliente.setEndereco(endereco);
-        clienteRepository.salvarCliente(cliente);
+        mockMvc.perform(MockMvcRequestBuilders.post("/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                .content(clienteJson));
 
-        mockMvc.perform(MockMvcRequestBuilders.delete("/clientes/" + cliente.getId()))
+        Optional<Cliente> cliente = clienteRepository.buscarClientePorEmail("cornelio@example.com");
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/clientes/" + cliente.get().getId())
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtToken)
+                )
                 .andExpect(status().isNoContent());
     }
 }
